7839424b658c0db94784dcd4ada0b2e48fb537b9
stephan <sg@sense-media.de>
2018-05-30 16:25:24 +0200

    updated to newest version 0.92.0

--- Source/OIDAuthState.m
+++ Source/OIDAuthState.m
@@ -104,14 +104,14 @@ static const NSUInteger kExpiryTimeTolerance = 60;
 
 #pragma mark - Convenience initializers
 
-+ (id<OIDAuthorizationFlowSession>)
++ (id<OIDExternalUserAgentSession, OIDAuthorizationFlowSession>)
     authStateByPresentingAuthorizationRequest:(OIDAuthorizationRequest *)authorizationRequest
-                                UICoordinator:(id<OIDAuthorizationUICoordinator>)UICoordinator
+                            externalUserAgent:(id<OIDExternalUserAgent>)externalUserAgent
                                      callback:(OIDAuthStateAuthorizationCallback)callback {
   // presents the authorization request
-  id<OIDAuthorizationFlowSession> authFlowSession = [OIDAuthorizationService
+  id<OIDExternalUserAgentSession, OIDAuthorizationFlowSession> authFlowSession = [OIDAuthorizationService
       presentAuthorizationRequest:authorizationRequest
-                    UICoordinator:UICoordinator
+                externalUserAgent:externalUserAgent
                          callback:^(OIDAuthorizationResponse *_Nullable authorizationResponse,
                                     NSError *_Nullable authorizationError) {
                            // inspects response and processes further if needed (e.g. authorization
@@ -157,7 +157,7 @@ static const NSUInteger kExpiryTimeTolerance = 60;
     OID_UNAVAILABLE_USE_INITIALIZER(@selector(initWithAuthorizationResponse:tokenResponse:));
 
 /*! @brief Creates an auth state from an authorization response.
-    @param response The authorization response.
+    @param authorizationResponse The authorization response.
  */
 - (instancetype)initWithAuthorizationResponse:(OIDAuthorizationResponse *)authorizationResponse {
   return [self initWithAuthorizationResponse:authorizationResponse tokenResponse:nil];
@@ -165,7 +165,7 @@ static const NSUInteger kExpiryTimeTolerance = 60;
 
 
 /*! @brief Designated initializer.
-    @param response The authorization response.
+    @param authorizationResponse The authorization response.
     @discussion Creates an auth state from an authorization response and token response.
  */
 - (instancetype)initWithAuthorizationResponse:(OIDAuthorizationResponse *)authorizationResponse
@@ -478,25 +478,25 @@ static const NSUInteger kExpiryTimeTolerance = 60;
     dispatch_async(dispatch_get_main_queue(), ^() {
       // update OIDAuthState based on response
       if (response) {
-        _needsTokenRefresh = NO;
+        self->_needsTokenRefresh = NO;
         [self updateWithTokenResponse:response error:nil];
       } else {
         if (error.domain == OIDOAuthTokenErrorDomain) {
-          _needsTokenRefresh = NO;
+          self->_needsTokenRefresh = NO;
           [self updateWithAuthorizationError:error];
         } else {
-          if ([_errorDelegate respondsToSelector:
+          if ([self->_errorDelegate respondsToSelector:
               @selector(authState:didEncounterTransientError:)]) {
-            [_errorDelegate authState:self didEncounterTransientError:error];
+            [self->_errorDelegate authState:self didEncounterTransientError:error];
           }
         }
       }
 
       // nil the pending queue and process everything that was queued up
       NSArray *actionsToProcess;
-      @synchronized(_pendingActionsSyncObject) {
-        actionsToProcess = _pendingActions;
-        _pendingActions = nil;
+      @synchronized(self->_pendingActionsSyncObject) {
+        actionsToProcess = self->_pendingActions;
+        self->_pendingActions = nil;
       }
       for (OIDAuthStateAction actionToProcess in actionsToProcess) {
         actionToProcess(self.accessToken, self.idToken, error);
